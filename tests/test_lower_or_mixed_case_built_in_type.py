d51ca00e1f36963febd45932a103f22851479a88
d4nuu8 <d4nuu8@gmail.com>
2019-03-02 17:17:17 +0100

    Add test for "wrong-case-type" rule

---
+++ tests/test_lower_or_mixed_case_built_in_type.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from importlib import reload
+
+from krllint import config
+from krllint.reporter import Category, MemoryReporter
+from krllint.linter import _create_arg_parser, Linter
+
+class LowerOrMixedCaseBuiltInTestCase(TestCase):
+    TEST_INPUT = ["Int\n"]
+    FIXED_INPUT = ["INT\n"]
+
+    def test_rule_without_fix(self):
+        cli_args = _create_arg_parser().parse_args(["test_rule_without_fix"])
+        reload(config)
+        config.REPORTER = MemoryReporter
+        linter = Linter(cli_args, config)
+        lines, reporter = linter.lint_lines("test_rule_without_fix", self.TEST_INPUT)
+
+        self.assertEqual(reporter.found_issues[Category.CONVENTION], 0)
+        self.assertEqual(reporter.found_issues[Category.REFACTOR], 0)
+        self.assertEqual(reporter.found_issues[Category.WARNING], 1)
+        self.assertEqual(reporter.found_issues[Category.ERROR], 0)
+        self.assertEqual(reporter.found_issues[Category.FATAL], 0)
+
+        self.assertEqual(lines, self.TEST_INPUT)
+
+        self.assertEqual(reporter.messages[0].line_number, 0)
+        self.assertEqual(reporter.messages[0].column, 0)
+        self.assertEqual(reporter.messages[0].message, "lower or mixed case built-in type")
+        self.assertEqual(reporter.messages[0].code, "wrong-case-type")
+
+    def test_rule_with_fix(self):
+        cli_args = _create_arg_parser().parse_args(["--fix", "test_rule_with_fix"])
+        reload(config)
+        config.REPORTER = MemoryReporter
+        linter = Linter(cli_args, config)
+        lines, _ = linter.lint_lines("test_rule_with_fix", self.TEST_INPUT)
+
+        self.assertEqual(lines, self.FIXED_INPUT)
