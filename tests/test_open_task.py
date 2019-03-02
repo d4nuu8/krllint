5a6405f08a4a9dd461b985cc1725339b4b9fa56d
d4nuu8 <d4nuu8@gmail.com>
2019-03-02 17:21:07 +0100

    Add test for "open-task" rule

---
+++ tests/test_open_task.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from importlib import reload
+
+from krllint import config
+from krllint.reporter import Category, MemoryReporter
+from krllint.linter import _create_arg_parser, Linter
+
+class OpenTaskTestCase(TestCase):
+    TEST_INPUT = [";TODO This is a open task\n"]
+
+    def test_rule_without_fix(self):
+        cli_args = _create_arg_parser().parse_args(["test_rule_without_fix"])
+        reload(config)
+        config.REPORTER = MemoryReporter
+        linter = Linter(cli_args, config)
+        lines, reporter = linter.lint_lines("test_rule_without_fix", self.TEST_INPUT)
+
+        self.assertEqual(reporter.found_issues[Category.CONVENTION], 0)
+        self.assertEqual(reporter.found_issues[Category.REFACTOR], 0)
+        self.assertEqual(reporter.found_issues[Category.WARNING], 1)
+        self.assertEqual(reporter.found_issues[Category.ERROR], 0)
+        self.assertEqual(reporter.found_issues[Category.FATAL], 0)
+
+        self.assertEqual(lines, self.TEST_INPUT)
+
+        self.assertEqual(reporter.messages[0].line_number, 0)
+        self.assertEqual(reporter.messages[0].column, 0)
+        self.assertEqual(reporter.messages[0].message, "complete open task (This is a open task)")
+        self.assertEqual(reporter.messages[0].code, "open-task")
+
+    def test_rule_with_fix(self):
+        cli_args = _create_arg_parser().parse_args(["--fix", "test_rule_with_fix"])
+        reload(config)
+        config.REPORTER = MemoryReporter
+        linter = Linter(cli_args, config)
+        lines, _ = linter.lint_lines("test_rule_with_fix", self.TEST_INPUT)
+
+        self.assertEqual(lines, self.TEST_INPUT)
